4eaaccc8b08495c0d083d84f4cc16a6657368387
Jakob Warnow <jakob.warnow@student.htw-berlin.de>
2017-10-24 14:49:11 +0200

    sa

--- src/frontend/scripts/app.js
+++ src/frontend/scripts/app.js
@@ -2,11 +2,9 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { ReMainCarsten } from './reMainCarsten';
 import { ReMain } from './reMain';
-import { ReMainRaum113 } from './reMainRaum113';
+// import { ReMainRaum113 } from './reMainRaum113';
 
 if ( window.app === 'carsten' )
   ReactDOM.render( <ReMainCarsten />, document.getElementById( 'main' ) );
-else if ( window.app === 'raum113' )
-  ReactDOM.render( <ReMainRaum113 />, document.getElementById( 'main' ) );
 else
   ReactDOM.render( <ReMain />, document.getElementById( 'main' ) );
